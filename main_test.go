1e538bf2f89f240e522db686b54ff3436bf4b9ca
Manuel de la Peña <mdelapenya@gmail.com>
2021-11-17 19:17:46 +0100

    chore: update tests

--- main_test.go
+++ main_test.go
@@ -15,53 +15,79 @@ func init() {
 	originalServiceNameFlag = serviceNameFlag
 }
 
-func Test_GetServiceName(t *testing.T) {
-	t.Run("Read from environment", func(t *testing.T) {
-		t.Run("Without environment variable retrieves fallback", func(t *testing.T) {
-			os.Unsetenv("OTEL_SERVICE_NAME")
-			defer resetEnvironment()
+func Test_GetServiceVariable(t *testing.T) {
+	var otlpTests = []struct {
+		fallback       string
+		getFn          func() string
+		setFlag        func()
+		variableSuffix string
+	}{
+		{
+			fallback: Junit2otlp,
+			getFn:    getOtlpServiceName,
+			setFlag: func() {
+				serviceNameFlag = "this-is-a-flag"
+			},
+			variableSuffix: "SERVICE_NAME",
+		},
+		{
+			fallback: "",
+			getFn:    getOtlpServiceVersion,
+			setFlag: func() {
+				serviceVersionFlag = "this-is-a-flag"
+			},
+			variableSuffix: "SERVICE_VERSION",
+		},
+	}
 
-			srvName := getOtlpServiceName()
+	for _, otlpotlpTest := range otlpTests {
+		t.Run("Read "+otlpotlpTest.variableSuffix+" from environment", func(t *testing.T) {
+			t.Run("Without environment variable retrieves fallback", func(t *testing.T) {
+				os.Unsetenv("OTEL_" + otlpotlpTest.variableSuffix)
+				defer resetEnvironment(otlpotlpTest.variableSuffix)
 
-			assert.Equal(t, Junit2otlp, srvName)
+				actualValue := otlpotlpTest.getFn()
+
+				assert.Equal(t, otlpotlpTest.fallback, actualValue)
+			})
+
+			t.Run("With environment variable retrieves the variable", func(t *testing.T) {
+				os.Setenv("OTEL_"+otlpotlpTest.variableSuffix, "foobar")
+				defer resetEnvironment(otlpotlpTest.variableSuffix)
+
+				actualValue := otlpotlpTest.getFn()
+
+				assert.Equal(t, "foobar", actualValue)
+			})
 		})
 
-		t.Run("With environment variable retrieves the variable", func(t *testing.T) {
-			os.Setenv("OTEL_SERVICE_NAME", "foobar")
-			defer resetEnvironment()
+		t.Run("Read "+otlpotlpTest.variableSuffix+" from command line flag", func(t *testing.T) {
+			t.Run("Without environment variable retrieves the flag", func(t *testing.T) {
+				os.Unsetenv("OTEL_" + otlpotlpTest.variableSuffix)
+				otlpotlpTest.setFlag()
+				defer resetEnvironment(otlpotlpTest.variableSuffix)
 
-			srvName := getOtlpServiceName()
+				actualValue := otlpotlpTest.getFn()
 
-			assert.Equal(t, "foobar", srvName)
+				assert.Equal(t, "this-is-a-flag", actualValue)
+			})
+
+			t.Run("With environment variable retrieves the variable", func(t *testing.T) {
+				os.Setenv("OTEL_"+otlpotlpTest.variableSuffix, "foobar")
+				otlpotlpTest.setFlag()
+				defer resetEnvironment(otlpotlpTest.variableSuffix)
+
+				actualValue := otlpotlpTest.getFn()
+
+				assert.Equal(t, "foobar", actualValue)
+				assert.Equal(t, "foobar", os.Getenv("OTEL_"+otlpotlpTest.variableSuffix))
+			})
 		})
-	})
-
-	t.Run("Read from command line flag", func(t *testing.T) {
-		t.Run("Without environment variable retrieves the flag", func(t *testing.T) {
-			os.Unsetenv("OTEL_SERVICE_NAME")
-			serviceNameFlag = "this-is-a-flag"
-			defer resetEnvironment()
-
-			srvName := getOtlpServiceName()
-
-			assert.Equal(t, "this-is-a-flag", srvName)
-		})
-
-		t.Run("With environment variable retrieves the variable", func(t *testing.T) {
-			os.Setenv("OTEL_SERVICE_NAME", "foobar")
-			serviceNameFlag = "this-is-a-flag"
-			defer resetEnvironment()
-
-			srvName := getOtlpServiceName()
-
-			assert.Equal(t, "foobar", srvName)
-			assert.Equal(t, "foobar", os.Getenv("OTEL_SERVICE_NAME"))
-		})
-	})
+	}
 }
 
-func resetEnvironment() {
-	os.Setenv("OTEL_SERVICE_NAME", originalEnvVar)
+func resetEnvironment(variableSuffix string) {
+	os.Setenv("OTEL_"+variableSuffix, originalEnvVar)
 
 	serviceNameFlag = originalServiceNameFlag
 }
