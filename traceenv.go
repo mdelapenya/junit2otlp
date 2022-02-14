e4af470d95818eb87cf87aa98d7babf43d55a4be
Manuel de la Peña <mdelapenya@gmail.com>
2022-02-14 18:40:33 +0100

    chore: remove deprecated content

--- traceenv.go
+++ traceenv.go
@@ -23,17 +23,7 @@ func initOtelContext(ctx context.Context) context.Context {
 		return tc.Extract(ctx, &textMap{parent: parent, state: state})
 	}
 
-	// deprecated: removed in v0.11.0
-	// previously defined in https://github.com/open-telemetry/opentelemetry-swift/blob/4ea467ed4b881d7329bf2254ca7ed7f2d9d6e1eb/Sources/OpenTelemetrySdk/Trace/Propagation/EnvironmentContextPropagator.swift#L14-L15
-	parent = os.Getenv("OTEL_TRACE_PARENT")
-	state = os.Getenv("OTEL_TRACE_STATE")
-
-	if parent == "" {
-		return ctx
-	}
-
-	tc := propagation.TraceContext{}
-	return tc.Extract(ctx, &textMap{parent: parent, state: state})
+	return ctx
 }
 
 type textMap struct {
