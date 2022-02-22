4af5aaf7bd82293276f8093641edb344ba78c6e8
Manuel de la Peña <mdelapenya@gmail.com>
2022-02-22 18:13:45 +0100

    fix: mock github variables for each SCM context

--- git_test.go
+++ git_test.go
@@ -211,12 +211,21 @@ func TestCheckGitContext(t *testing.T) {
 	originalGithubSha := os.Getenv("GITHUB_SHA")
 	originalGitlabBranch := os.Getenv("CI_COMMIT_BRANCH")
 
+	unsetGithub := func() {
+		os.Setenv("GITHUB_SHA", "")
+	}
+
+	restoreSCMContextsFn := func() {
+		os.Setenv("GITHUB_SHA", originalGithubSha)
+		os.Setenv("CI_COMMIT_BRANCH", originalGitlabBranch)
+	}
+
 	t.Run("Github", func(t *testing.T) {
 		// Prepare Github
 		originalBaseRef := os.Getenv("GITHUB_BASE_REF")
 		originalHeadRef := os.Getenv("GITHUB_HEAD_REF")
 		restoreGithubFn := func() {
-			os.Setenv("GITHUB_SHA", originalGithubSha)
+			restoreSCMContextsFn()
 			os.Setenv("GITHUB_BASE_REF", originalBaseRef)
 			os.Setenv("GITHUB_HEAD_REF", originalHeadRef)
 		}
@@ -269,10 +278,6 @@ func TestCheckGitContext(t *testing.T) {
 	})
 
 	t.Run("Jenkins", func(t *testing.T) {
-		if originalGithubSha != "" {
-			t.Skip("Tests skipped when running on Github")
-		}
-
 		// prepare Jenkins
 		jenkinsChangeID := os.Getenv("CHANGE_ID")
 		jenkinsGitCommit := os.Getenv("GIT_COMMIT")
@@ -280,6 +285,7 @@ func TestCheckGitContext(t *testing.T) {
 		jenkinsChangeTargetName := os.Getenv("CHANGE_TARGET")
 		jenkinsURL := os.Getenv("JENKINS_URL")
 		restoreJenkinsFn := func() {
+			restoreSCMContextsFn()
 			os.Setenv("CHANGE_ID", jenkinsChangeID)
 			os.Setenv("GIT_COMMIT", jenkinsGitCommit)
 			os.Setenv("BRANCH_NAME", jenkinsChangeBranchName)
@@ -291,6 +297,7 @@ func TestCheckGitContext(t *testing.T) {
 		testBranch := "mybranch"
 
 		t.Run("Running for Branches", func(t *testing.T) {
+			unsetGithub()
 			os.Setenv("JENKINS_URL", "http://jenkins.local")
 			os.Setenv("GIT_COMMIT", testSha)
 			os.Setenv("CHANGE_ID", "")
@@ -307,6 +314,7 @@ func TestCheckGitContext(t *testing.T) {
 		})
 
 		t.Run("Running for Pull Requests", func(t *testing.T) {
+			unsetGithub()
 			os.Setenv("JENKINS_URL", "http://jenkins.local")
 			os.Setenv("GIT_COMMIT", testSha)
 			os.Setenv("CHANGE_ID", "PR-123")
@@ -324,22 +332,19 @@ func TestCheckGitContext(t *testing.T) {
 	})
 
 	t.Run("Gitlab", func(t *testing.T) {
-		if originalGithubSha != "" {
-			t.Skip("Tests skipped when running on Github")
-		}
-
 		// prepare Gitlab
 		gitlabRefName := os.Getenv("CI_COMMIT_REF_NAME")
 		originalSourceBranchSha := os.Getenv("CI_MERGE_REQUEST_SOURCE_BRANCH_SHA")
 		originalTargetBranchName := os.Getenv("CI_MERGE_REQUEST_TARGET_BRANCH_NAME")
 		restoreGitlabFn := func() {
-			os.Setenv("CI_COMMIT_BRANCH", originalGitlabBranch)
+			restoreSCMContextsFn()
 			os.Setenv("CI_COMMIT_REF_NAME", gitlabRefName)
 			os.Setenv("CI_MERGE_REQUEST_SOURCE_BRANCH_SHA", originalSourceBranchSha)
 			os.Setenv("CI_MERGE_REQUEST_TARGET_BRANCH_NAME", originalTargetBranchName)
 		}
 
 		t.Run("Running for Branches", func(t *testing.T) {
+			unsetGithub()
 			os.Setenv("CI_COMMIT_BRANCH", "branch")
 			os.Setenv("CI_COMMIT_REF_NAME", "branch")
 			os.Setenv("CI_MERGE_REQUEST_SOURCE_BRANCH_SHA", "0123456")
@@ -355,6 +360,7 @@ func TestCheckGitContext(t *testing.T) {
 		})
 
 		t.Run("Running for Merge Requests", func(t *testing.T) {
+			unsetGithub()
 			os.Setenv("CI_COMMIT_REF_NAME", "branch")
 			os.Setenv("CI_MERGE_REQUEST_SOURCE_BRANCH_SHA", "0123456")
 			os.Setenv("CI_MERGE_REQUEST_TARGET_BRANCH_NAME", "main")
@@ -370,15 +376,13 @@ func TestCheckGitContext(t *testing.T) {
 	})
 
 	t.Run("Local machine", func(t *testing.T) {
-		if originalGithubSha != "" {
-			t.Skip("Tests skipped when running on Github")
-		}
-
 		t.Run("Running with TARGET_BRANCH", func(t *testing.T) {
+			unsetGithub()
 			os.Setenv("BRANCH", "foo")
 			os.Setenv("TARGET_BRANCH", "main")
 			defer os.Unsetenv("TARGET_BRANCH")
 			defer os.Unsetenv("BRANCH")
+			defer restoreSCMContextsFn()
 
 			gitCtx := checkGiContext()
 			assert.Equal(t, "", gitCtx.Commit)
@@ -389,8 +393,10 @@ func TestCheckGitContext(t *testing.T) {
 		})
 
 		t.Run("Running without TARGET_BRANCH", func(t *testing.T) {
+			unsetGithub()
 			os.Setenv("BRANCH", "foo")
 			defer os.Unsetenv("BRANCH")
+			restoreSCMContextsFn()
 
 			gitCtx := checkGiContext()
 			assert.Equal(t, "", gitCtx.Commit)
@@ -402,6 +408,9 @@ func TestCheckGitContext(t *testing.T) {
 	})
 
 	t.Run("Empty SCM context", func(t *testing.T) {
+		unsetGithub()
+		defer restoreSCMContextsFn()
+
 		gitCtx := checkGiContext()
 		assert.Nil(t, gitCtx)
 	})
