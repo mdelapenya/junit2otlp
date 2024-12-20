0f2103ed917f22748318a45c04b73199d758f198
Ryan Old <ryanrolds@gmail.com>
2024-12-20 09:10:09 -0800

    Report testcase metrics and attribute adjustments

--- semconv.go
+++ semconv.go
@@ -19,18 +19,29 @@ const (
 	ScmRepository = "scm.repository"
 	ScmType       = "scm.type"
 
-	// suite keys
+	// test suite metrics
 	FailedTestsCount  = "tests.suite.failed"
 	ErrorTestsCount   = "tests.suite.error"
 	PassedTestsCount  = "tests.suite.passed"
 	SkippedTestsCount = "tests.suite.skipped"
-	TestsDuration     = "tests.suite.duration"
-	TestsSuiteName    = "tests.suite.suitename"
-	TestsSystemErr    = "tests.suite.systemerr"
-	TestsSystemOut    = "tests.suite.systemout"
 	TotalTestsCount   = "tests.suite.total"
+	TestsDuration     = "tests.suite.duration"
+	TestsDurationHist = "tests.suite.duration.histogram"
 
-	// test keys
+	// test case metrics
+	CaseFailedCount  = "tests.case.failed"
+	CaseErrorCount   = "tests.case.error"
+	CasePassedCount  = "tests.case.passed"
+	CaseSkippedCount = "tests.case.skipped"
+	CaseDuration     = "tests.case.duration"
+	CaseDurationHist = "tests.case.duration.histogram"
+
+	// test suite keys
+	TestsSuiteName = "tests.suite.suitename"
+	TestsSystemErr = "tests.suite.systemerr"
+	TestsSystemOut = "tests.suite.systemout"
+
+	// test case keys
 	TestClassName = "tests.case.classname"
 	TestDuration  = "tests.case.duration"
 	TestError     = "tests.case.error"
